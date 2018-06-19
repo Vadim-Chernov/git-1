dccede89df3a8049fca5dc7e113e173087937cc8
Vadim-Chernov <chernov.vr@gmail.com>
2018-06-19 14:26:49 +0700

    Update HelloWorld2.java

--- src/cvr/git/HelloWorld2.java
+++ src/cvr/git/HelloWorld2.java
@@ -3,6 +3,6 @@ package cvr.git;
 public class HelloWorld2 {
   public static void main(String[] args) {
     System.out.println("Hello, World2!!");
-
+//////////////////////////////////////////
   }
 }
